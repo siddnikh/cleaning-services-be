42022fa6c27eff940b9aa6b679e5919b37530d59
siddnikh <mailtosiddnikh@gmail.com>
2024-09-07 14:20:18 +0530

    Added otp routes to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { logger } = require('../config/logger');
-const { signup, login } = require('../controllers/auth');
+const { signup, login, requestOTP, verifyOTP } = require('../controllers/auth');
 
 const authRouter = express.Router();
 
@@ -9,7 +9,7 @@ authRouter.post('/signup', async (req, res) => {
         await signup(req, res);
     } catch (error) {
         logger.error(`Error in /signup: ${error.message}`);
-        const status = error.status || 500;
+        const status = error.statusCode || 500;
         res.status(status).json({ error: error.message });
     }
 });
@@ -19,7 +19,38 @@ authRouter.post('/login', async (req, res) => {
         await login(req, res);
     } catch (error) {
         logger.error(`Error in /login: ${error.message}`);
-        const status = error.status || 500;
+        const status = error.statusCode || 500;
+        res.status(status).json({ error: error.message });
+    }
+});
+
+authRouter.post('/request-otp', async (req, res) => {
+    try {
+        const otp = await generateOTP(req, res);
+        await sendOTP(req, res, otp);
+    } catch (error) {
+        logger.error(`Error generating OTP: ${error.message}`);
+        const status = error.statusCode || 500;
+        res.status(status).json({ error: error.message });
+    }
+});
+
+authRouter.post('/verify-otp', async (req, res) => {
+    try {
+        await verifyOTP(req, res);
+    } catch (error) {
+        logger.error(`Error in verifying OTP: ${error.message}`);
+        const status = error.statusCode || 500;
+        res.status(status).json({ error: error.message });
+    }
+})
+
+authRouter.post('/is-otp-verified', async (req, res) => {
+    try {
+        await isUserOTPVerified(req, res);
+    } catch (error) {
+        logger.error(`Error in checking OTP verification: ${error.message}`);
+        const status = error.statusCode || 500;
         res.status(status).json({ error: error.message });
     }
 });
