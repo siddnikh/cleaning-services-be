40adb8ba10e1019caf0ceb4537be9331c5f9e5f7
siddnikh <mailtosiddnikh@gmail.com>
2024-09-09 15:13:38 +0530

    provider querying controller

---
+++ controllers/providers.js
@@ -0,0 +1,85 @@
+const { Op } = require('sequelize');
+const { Profile } = require('../models/User');
+
+const getNearestProviders = async (req, res) => {
+  try {
+    const nearbyProviders = await Profile.findAll({
+      where: {
+        type: 'Provider',
+        location: {
+          [Op.ne]: null
+        }
+      },
+      attributes: [
+        'id',
+        'name',
+        'services',
+        'rating',
+        [
+          Profile.sequelize.fn(
+            'ST_Distance',
+            Profile.sequelize.col('location'),
+            req.user.location
+          ),
+          'distance'
+        ]
+      ],
+      order: [
+        [
+          Profile.sequelize.fn('ST_Distance', Profile.sequelize.col('location'), req.user.location),
+          'ASC'
+        ]
+      ],
+      limit: 10
+    });
+
+    res.json(nearbyProviders);
+  } catch (error) {
+    const err = new Error(error.message);
+    err.statusCode = 500;
+    throw err;
+  }
+};
+
+const getHighestRatedProviders = async (req, res) => {
+  try {
+    const highestRatedProviders = await Profile.findAll({
+      where: {
+        type: 'Provider',
+        rating: {
+          [Op.ne]: null
+        }
+      },
+      attributes: [
+        'id',
+        'name',
+        'services',
+        'rating',
+        [
+          Profile.sequelize.fn(
+            'ST_Distance',
+            Profile.sequelize.col('location'),
+            req.user.location
+          ),
+          'distance'
+        ]
+      ],
+      having: Profile.sequelize.literal('distance <= 50000'), // within a 50km radius
+      order: [
+        ['rating', 'DESC']
+      ],
+      limit: 20
+    });
+
+    res.json(highestRatedProviders);
+  } catch (error) {
+    const err = new Error(error.message);
+    err.statusCode = 500;
+    throw err;
+  }
+};
+
+module.exports = {
+  getNearestProviders,
+  getHighestRatedProviders
+};
