d1974255fb87ca932f1f988e0f09a8b88c990570
siddnikh <mailtosiddnikh@gmail.com>
2024-09-09 22:07:46 +0530

    fix: minor

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const { logger } = require('../config/logger');
-const { getUserProfile, getUserProfileByEmail, updateUserProfile, deleteUserProfile, verifyPin } = require('../controllers/user');
+const { getUser, getUserByEmail, updateUser, deleteUser, verifyPin } = require('../controllers/user');
 const { verifyUser } = require("../utils/jwtUtils");
 
 const userRouter = express.Router();
 
 userRouter.get('/me', verifyUser, async (req, res) => {
     try {
-        await getUserProfile(req, res);
+        await getUser(req, res);
     } catch (error) {
         logger.error(`Error in /me: ${error.message}`);
         const status = error.status || 500;
@@ -17,7 +17,7 @@ userRouter.get('/me', verifyUser, async (req, res) => {
 
 userRouter.get('/:email', verifyUser, async (req, res) => {
     try {
-        await getUserProfileByEmail(req, res);
+        await getUserByEmail(req, res);
     } catch (error) {
         logger.error(`Error in getting profile by email: ${error.message}`);
         const status = error.statusCode || 500;
@@ -27,7 +27,7 @@ userRouter.get('/:email', verifyUser, async (req, res) => {
 
 userRouter.post('/update', verifyUser, async (req, res) => {
     try {
-        await updateUserProfile(req, res);
+        await updateUser(req, res);
     } catch (error) {
         logger.error(`Error in /update: ${error.message}`);
         const status = error.statusCode || 500;
@@ -37,7 +37,7 @@ userRouter.post('/update', verifyUser, async (req, res) => {
 
 userRouter.delete('/delete', verifyUser, async (req, res) => {
     try {
-        await deleteUserProfile(req, res);
+        await deleteUser(req, res);
     } catch (error) {
         logger.error(`Error in /delete: ${error.message}`);
         const status = error.statusCode || 500;
